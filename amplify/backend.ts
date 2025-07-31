011ab44a5e8d41e1c9f930b0315e7b1e3cf2a321
peace4turtles <peace4turtle@gmail.com>
2025-07-31 14:58:51 -0500

    policy configured programatically - QA 1

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend, defineFunction } from '@aws-amplify/backend';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
@@ -6,10 +7,28 @@ import { data } from './data/resource';
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 export const backend = defineBackend({
+  auth,
+  data,
   helloFunction: defineFunction({
     entry: './functions/myFunction.ts',
     environment: {
       SANDWICH_TABLE_NAME: "Sandwich",
-    },
+    }
   }),
 });
+
+// Grant the function access to the Sandwich table
+backend.helloFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: [
+      'dynamodb:GetItem',
+      'dynamodb:PutItem',
+      'dynamodb:UpdateItem',
+      'dynamodb:DeleteItem',
+      'dynamodb:Scan',
+      'dynamodb:Query'
+    ],
+    resources: [backend.data.resources.tables["Sandwich"].tableArn]
+  })
+);
