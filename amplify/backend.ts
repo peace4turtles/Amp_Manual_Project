85e11c53291c8cc4fc81ee83a4793b7ee5f8e8d8
peace4turtles <peace4turtle@gmail.com>
2025-08-01 12:53:11 -0500

    adds initial API Gateway code

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,21 @@
 import { defineBackend, defineFunction } from '@aws-amplify/backend';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Effect, PolicyStatement, Policy } from 'aws-cdk-lib/aws-iam';
+import { Stack } from "aws-cdk-lib";
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  Cors,
+  LambdaIntegration,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { myAPIFunction } from './functions/api-function.js/resource';
 
 export const backend = defineBackend({
   auth,
   data,
+  myAPIFunction,
   helloFunction: defineFunction({
     entry: './functions/myFunction.ts',
     environment: {
@@ -14,7 +24,6 @@ export const backend = defineBackend({
   }),
 });
 
-// Grant the function access to list tables (account level)
 backend.helloFunction.resources.lambda.addToRolePolicy(
   new PolicyStatement({
     effect: Effect.ALLOW,
@@ -23,7 +32,6 @@ backend.helloFunction.resources.lambda.addToRolePolicy(
   })
 );
 
-// Grant the function access to Sandwich tables specifically
 backend.helloFunction.resources.lambda.addToRolePolicy(
   new PolicyStatement({
     effect: Effect.ALLOW,
@@ -38,3 +46,89 @@ backend.helloFunction.resources.lambda.addToRolePolicy(
     resources: [`arn:aws:dynamodb:*:*:table/Sandwich*`]
   })
 );
+
+// create a new API stack
+const apiStack = backend.createStack("api-stack");
+
+const myRestApi = new RestApi(apiStack, "RestApi", {
+  restApiName: "myRestApi",
+  deploy: true,
+  deployOptions: {
+    stageName: "dev",
+  },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_ORIGINS, // Restrict this to domains you trust
+    allowMethods: Cors.ALL_METHODS, // Specify only the methods you need to allow
+    allowHeaders: Cors.DEFAULT_HEADERS, // Specify only the headers you need to allow
+  },
+});
+
+const lambdaIntegration = new LambdaIntegration(
+  backend.myAPIFunction.resources.lambda
+);
+
+// create a new resource path with IAM authorization
+const itemsPath = myRestApi.root.addResource("items", {
+  defaultMethodOptions: {
+    authorizationType: AuthorizationType.IAM,
+  },
+});
+
+// add methods you would like to create to the resource path
+itemsPath.addMethod("GET", lambdaIntegration);
+itemsPath.addMethod("POST", lambdaIntegration);
+itemsPath.addMethod("DELETE", lambdaIntegration);
+itemsPath.addMethod("PUT", lambdaIntegration);
+
+// add a proxy resource path to the API
+itemsPath.addProxy({
+  anyMethod: true,
+  defaultIntegration: lambdaIntegration,
+});
+
+// create a new Cognito User Pools authorizer
+const cognitoAuth = new CognitoUserPoolsAuthorizer(apiStack, "CognitoAuth", {
+  cognitoUserPools: [backend.auth.resources.userPool],
+});
+
+// create a new resource path with Cognito authorization
+const booksPath = myRestApi.root.addResource("cognito-auth-path");
+booksPath.addMethod("GET", lambdaIntegration, {
+  authorizationType: AuthorizationType.COGNITO,
+  authorizer: cognitoAuth,
+});
+
+// create a new IAM policy to allow Invoke access to the API
+const apiRestPolicy = new Policy(apiStack, "RestApiPolicy", {
+  statements: [
+    new PolicyStatement({
+      actions: ["execute-api:Invoke"],
+      resources: [
+        `${myRestApi.arnForExecuteApi("*", "/items", "dev")}`,
+        `${myRestApi.arnForExecuteApi("*", "/items/*", "dev")}`,
+        `${myRestApi.arnForExecuteApi("*", "/cognito-auth-path", "dev")}`,
+      ],
+    }),
+  ],
+});
+
+// attach the policy to the authenticated and unauthenticated IAM roles
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
+  apiRestPolicy
+);
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
+  apiRestPolicy
+);
+
+// add outputs to the configuration file
+backend.addOutput({
+  custom: {
+    API: {
+      [myRestApi.restApiName]: {
+        endpoint: myRestApi.url,
+        region: Stack.of(myRestApi).region,
+        apiName: myRestApi.restApiName,
+      },
+    },
+  },
+});
---
+++ amplify/functions/api-function.js/resource.ts
@@ -0,0 +1,5 @@
+import { defineFunction } from "@aws-amplify/backend";
+
+export const myAPIFunction = defineFunction({
+  name: "my-new-api-function",
+});
