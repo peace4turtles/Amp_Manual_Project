cbcad420589dd9777d15fa111ab59665b0850c0a
peace4turtles <peace4turtle@gmail.com>
2025-08-06 10:40:00 -0500

    api cognito QA

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -8,6 +8,7 @@ import {
   CognitoUserPoolsAuthorizer,
   LambdaIntegration,
   RestApi,
+  Cors,
 } from "aws-cdk-lib/aws-apigateway";
 import { myAPIFunction } from './functions/api-function.js/resource';
 import { storage } from './storage/resource';
@@ -50,78 +51,89 @@ backend.helloFunction.resources.lambda.addToRolePolicy(
 
 const apiStack = backend.createStack("api-stack");
 
+// const myRestApi = new RestApi(apiStack, "RestApi", {
+//   restApiName: "myRestApi",
+// });
+
+// const lambdaIntegration = new LambdaIntegration(
+//   backend.helloFunction.resources.lambda
+// );
+
+// ---------------------------------------------
+
+// create a new REST API
 const myRestApi = new RestApi(apiStack, "RestApi", {
   restApiName: "myRestApi",
   deploy: true,
   deployOptions: {
     stageName: "dev",
   },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_ORIGINS, // Restrict this to domains you trust
+    allowMethods: Cors.ALL_METHODS, // Specify only the methods you need to allow
+    allowHeaders: Cors.DEFAULT_HEADERS, // Specify only the headers you need to allow
+  },
 });
 
+// create a new Lambda integration
 const lambdaIntegration = new LambdaIntegration(
   backend.helloFunction.resources.lambda
 );
 
-const cognitoAuthorizer = new CognitoUserPoolsAuthorizer(apiStack, 'CognitoAuthorizer', {
+// create a new resource path with IAM authorization
+const itemsPath = myRestApi.root.addResource("items", {
+  defaultMethodOptions: {
+    authorizationType: AuthorizationType.IAM,
+  },
+});
+
+// add methods you would like to create to the resource path
+itemsPath.addMethod("GET", lambdaIntegration);
+itemsPath.addMethod("POST", lambdaIntegration);
+itemsPath.addMethod("DELETE", lambdaIntegration);
+itemsPath.addMethod("PUT", lambdaIntegration);
+
+// add a proxy resource path to the API
+itemsPath.addProxy({
+  anyMethod: true,
+  defaultIntegration: lambdaIntegration,
+});
+
+// create a new Cognito User Pools authorizer
+const cognitoAuth = new CognitoUserPoolsAuthorizer(apiStack, "CognitoAuth", {
   cognitoUserPools: [backend.auth.resources.userPool],
 });
 
-const items = myRestApi.root.addResource("items");
-
-items.addCorsPreflight({
-  allowOrigins: ['*'],
-  allowMethods: ['GET', 'POST', 'OPTIONS'],
-  allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-});
-
-items.addMethod("GET", lambdaIntegration, {
+// create a new resource path with Cognito authorization
+const booksPath = myRestApi.root.addResource("cognito-auth-path");
+booksPath.addMethod("GET", lambdaIntegration, {
   authorizationType: AuthorizationType.COGNITO,
-  authorizer: cognitoAuthorizer,
-  methodResponses: [{
-    statusCode: '200',
-    responseParameters: {
-      'method.response.header.Access-Control-Allow-Origin': true,
-      'method.response.header.Access-Control-Allow-Headers': true,
-      'method.response.header.Access-Control-Allow-Methods': true,
-    },
-  }],
+  authorizer: cognitoAuth,
 });
 
-const publicItems = myRestApi.root.addResource("public");
-
-publicItems.addCorsPreflight({
-  allowOrigins: ['*'],
-  allowMethods: ['GET', 'POST', 'OPTIONS'],
-  allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-});
-
-publicItems.addMethod("GET", lambdaIntegration, {
-  authorizationType: AuthorizationType.NONE,
-  methodResponses: [{
-    statusCode: '200',
-    responseParameters: {
-      'method.response.header.Access-Control-Allow-Origin': true,
-      'method.response.header.Access-Control-Allow-Headers': true,
-      'method.response.header.Access-Control-Allow-Methods': true,
-    },
-  }],
-});
-
+// create a new IAM policy to allow Invoke access to the API
 const apiRestPolicy = new Policy(apiStack, "RestApiPolicy", {
   statements: [
     new PolicyStatement({
       actions: ["execute-api:Invoke"],
       resources: [
         `${myRestApi.arnForExecuteApi("*", "/items", "dev")}`,
-        `${myRestApi.arnForExecuteApi("*", "/public", "dev")}`,
+        `${myRestApi.arnForExecuteApi("*", "/items/*", "dev")}`,
+        `${myRestApi.arnForExecuteApi("*", "/cognito-auth-path", "dev")}`,
       ],
     }),
   ],
 });
 
-backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(apiRestPolicy);
-backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(apiRestPolicy);
+// attach the policy to the authenticated and unauthenticated IAM roles
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
+  apiRestPolicy
+);
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
+  apiRestPolicy
+);
 
+// add outputs to the configuration file
 backend.addOutput({
   custom: {
     API: {
