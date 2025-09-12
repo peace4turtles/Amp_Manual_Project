0dcc07cea7590d65696f395f8fe4ea7a3598f435
peace4turtles <peace4turtle@gmail.com>
2025-09-12 10:52:05 -0500

    back ups

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,192 +1,53 @@
-import { defineBackend, defineFunction } from '@aws-amplify/backend';
-import { Effect, PolicyStatement, Policy } from 'aws-cdk-lib/aws-iam';
-import { Stack, Duration } from "aws-cdk-lib";
+import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+
 import {
-  AuthorizationType,
-  CognitoUserPoolsAuthorizer,
-  LambdaIntegration,
-  RestApi,
-  Cors,
-} from "aws-cdk-lib/aws-apigateway";
-import { myAPIFunction } from './functions/api-function.js/resource';
-import { storage } from './storage/resource';
-import * as cognito from 'aws-cdk-lib/aws-cognito'
-import { CfnUserPoolGroup } from 'aws-cdk-lib/aws-cognito';
+  BackupPlan,
+  BackupPlanRule,
+  BackupResource,
+  BackupVault,
+} from "aws-cdk-lib/aws-backup";
+import { Schedule } from "aws-cdk-lib/aws-events";
+import { Duration } from "aws-cdk-lib/core";
 
-export const backend = defineBackend({
+const backend = defineBackend({
   auth,
-  data,
-  myAPIFunction,
-  helloFunction: defineFunction({
-    entry: './functions/myFunction.ts',
-    environment: {
-      TABLE_NAME: "Sandwich"
-    }
-  }),
-  storage
-});
-
-backend.helloFunction.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: ['dynamodb:ListTables'],
-    resources: ['*']
-  })
-);
-
-backend.helloFunction.resources.lambda.addToRolePolicy(
-  new PolicyStatement({
-    effect: Effect.ALLOW,
-    actions: [
-      'dynamodb:GetItem',
-      'dynamodb:PutItem',
-      'dynamodb:UpdateItem',
-      'dynamodb:DeleteItem',
-      'dynamodb:Scan',
-      'dynamodb:Query'
-    ],
-    resources: [`arn:aws:dynamodb:*:*:table/Sandwich*`]
-  })
-);
-
-const apiStack = backend.createStack("api-stack");
-
-// const myRestApi = new RestApi(apiStack, "RestApi", {
-//   restApiName: "myRestApi",
-// });
-
-// const lambdaIntegration = new LambdaIntegration(
-//   backend.helloFunction.resources.lambda
-// );
-
-// ---------------------------------------------
-
-// create a new REST API
-const myRestApi = new RestApi(apiStack, "RestApi", {
-  restApiName: "api-stack-EXPERIMENT",
-  deploy: true,
-  deployOptions: {
-    stageName: "dev",
-  },
-  defaultCorsPreflightOptions: {
-    allowOrigins: Cors.ALL_ORIGINS, // Restrict this to domains you trust
-    allowMethods: Cors.ALL_METHODS, // Specify only the methods you need to allow
-    allowHeaders: Cors.DEFAULT_HEADERS, // Specify only the headers you need to allow
-  },
-});
-
-// create a new Lambda integration
-// const lambdaIntegration = new LambdaIntegration(
-//   backend.helloFunction.resources.lambda
-// );
-
-const lambdaIntegration = new LambdaIntegration(
-  backend.myAPIFunction.resources.lambda
-);
-
-// create a new resource path with IAM authorization
-const itemsPath = myRestApi.root.addResource("items", {
-  defaultMethodOptions: {
-    authorizationType: AuthorizationType.IAM,
-  },
-});
-
-// add methods you would like to create to the resource path
-itemsPath.addMethod("GET", lambdaIntegration);
-itemsPath.addMethod("POST", lambdaIntegration);
-itemsPath.addMethod("DELETE", lambdaIntegration);
-itemsPath.addMethod("PUT", lambdaIntegration);
-
-
-// add a proxy resource path to the API
-itemsPath.addProxy({
-  anyMethod: true,
-  defaultIntegration: lambdaIntegration,
-  // defaultMethodOptions: {
-  //   authorizationType: AuthorizationType.COGNITO,
-  //   authorizer: cognitoAuth, // Add this line
-  // },
-});
-
-// create a new Cognito User Pools authorizer
-const cognitoAuth = new CognitoUserPoolsAuthorizer(apiStack, "CognitoAuth", {
-  cognitoUserPools: [backend.auth.resources.userPool], // Use the userPool you created below instead of backend.auth.resources.userPool
-});
-
-// create a new resource path with Cognito authorization
-const booksPath = myRestApi.root.addResource("cognito-auth-path");
-booksPath.addMethod("GET", lambdaIntegration, {
-  authorizationType: AuthorizationType.COGNITO,
-  authorizer: cognitoAuth, // Add this line
-});
-
-const userPool = new cognito.UserPool(apiStack, 'UserPool', {
-  selfSignUpEnabled: true,
-  signInAliases: {
-    email: true
-  },
-  lambdaTriggers: {
-    postConfirmation: backend.helloFunction.resources.lambda
-  }
-});
-
-const userPoolClient = userPool.addClient('UserPoolClient', {
-  authFlows: {
-    userPassword: true,
-    userSrp: true
-  },
-  accessTokenValidity: Duration.days(1),
-  idTokenValidity: Duration.days(1),
-  refreshTokenValidity: Duration.days(30)
-});
-
-// Create User Pool groups
-const adminGroup = new CfnUserPoolGroup(apiStack, 'AdminGroup', {
-  groupName: 'ADMIN',
-  userPoolId: userPool.userPoolId,
-  description: 'Admin users group',
-});
-
-const userGroup = new CfnUserPoolGroup(apiStack, 'UserGroup', {
-  groupName: 'USER',
-  userPoolId: userPool.userPoolId,
-  description: 'Regular users group',
+  data
 });
 
 
-// create a new IAM policy to allow Invoke access to the API
-const apiRestPolicy = new Policy(apiStack, "RestApiPolicy", {
-  statements: [
-    new PolicyStatement({
-      actions: ["execute-api:Invoke"],
-      resources: [
-        `${myRestApi.arnForExecuteApi("*", "/items", "dev")}`,
-        `${myRestApi.arnForExecuteApi("*", "/items/*", "dev")}`,
-        `${myRestApi.arnForExecuteApi("*", "/cognito-auth-path", "dev")}`,
-      ],
+const backupStack = backend.createStack("backup-stack");
+const myTables = Object.values(backend.data.resources.tables);
+
+
+const vault = new BackupVault(backupStack, "BackupVault", {
+  backupVaultName: "backup-vault",
+});
+
+
+const plan = new BackupPlan(backupStack, "BackupPlan", {
+  backupPlanName: "backup-plan",
+  backupVault: vault,
+});
+
+
+plan.addRule(
+  new BackupPlanRule({
+    deleteAfter: Duration.days(60),
+    ruleName: "backup-plan-rule",
+    scheduleExpression: Schedule.cron({
+      minute: "0",
+      hour: "0",
+      day: "*",
+      month: "*",
+      year: "*",
     }),
-  ],
-});
-
-// attach the policy to the authenticated and unauthenticated IAM roles
-backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
-  apiRestPolicy
-);
-backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
-  apiRestPolicy
+  })
 );
 
-// add outputs to the configuration file
-backend.addOutput({
-  custom: {
-    API: {
-      [myRestApi.restApiName]: {
-        endpoint: myRestApi.url,
-        region: Stack.of(myRestApi).region,
-        apiName: myRestApi.restApiName,
-      },
-    },
-  },
+
+plan.addSelection("BackupPlanSelection", {
+  resources: myTables.map((table) => BackupResource.fromDynamoDbTable(table)),
+  allowRestores: true,
 });
